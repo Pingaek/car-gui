5d3e419a5061fdbe8bec1bcc0bb7eb5523e34dc8
noasuke <noasuke@hotmail.com>
2026-03-05 16:19:28 +0700

    add more functions

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class MainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
     uic.loadUi('cms.ui', self)
+    self.id = 0
 
     self.tb_car.setColumnWidth(0, 50)
     self.tb_car.setColumnWidth(1, 150)
@@ -15,10 +16,99 @@ class MainWindow(QMainWindow):
     self.tb_car.setColumnWidth(3, 100)
     self.tb_car.setColumnWidth(4, 180)
 
+    self.show_all_cars()
     self.btn_add.clicked.connect(self.insert_car)
+    self.btn_search.clicked.connect(self.search_car)
+    self.btn_clear.clicked.connect(self.clear)
+    self.btn_update.clicked.connect(self.update_car)
+    self.btn_delete.clicked.connect(self.delete_car)
+    self.tb_car.cellClicked.connect(self.selected_row)
 
   def say_hi(self):
     QMessageBox.information(self, 'Information', 'Hello World!')
+
+  def update_car(self):
+    price = int(self.txt_price.text())
+    sql = 'update car set price = ? where id = ?'
+    values = (price, self.id)
+
+    row = cursor.execute(sql, values)
+    db.commit()
+
+    if row.rowcount>0:
+      QMessageBox.information(self, 
+                              'Information', 
+                              'Update car successful!')
+      self.show_all_cars()
+    else:
+      QMessageBox.warning(self, 
+                              'Warning', 
+                              'Unable to update car!')
+    self.clear()
+
+  def delete_car(self):
+    sql = 'delete from car where id = ?'
+    values = (self.id, )
+
+    row = cursor.execute(sql, values)
+    db.commit()
+
+    if row.rowcount>0:
+      QMessageBox.information(self, 
+                              'Information', 
+                              'Delete car successful!')
+      self.show_all_cars()
+    else:
+      QMessageBox.warning(self, 
+                              'Warning', 
+                              'Unable to delete car!')
+    self.clear()
+
+  def selected_row(self):
+    row = self.tb_car.currentRow()
+    self.id = self.tb_car.item(row, 0).text()
+    self.txt_brand.setText(self.tb_car.item(row, 1).text())
+    self.txt_model.setText(self.tb_car.item(row, 2).text())
+    self.txt_year.setText(self.tb_car.item(row, 3).text())
+    self.txt_price.setText(self.tb_car.item(row, 4).text())
+
+    self.btn_update.setEnabled(True)
+    self.btn_delete.setEnabled(True)
+    self.btn_add.setEnabled(False)
+
+    self.txt_brand.setEnabled(False)
+    self.txt_model.setEnabled(False)
+    self.txt_year.setEnabled(False)
+
+  def search_car(self):
+    brand = self.txt_search.text()
+    # print(brand)
+    sql = 'select * from car where brand like ?'
+    values = (f'%{brand}%', )
+
+    cars = cursor.execute(sql, values).fetchall()
+    self.show_cars(cars)
+
+    self.txt_search.setText('')
+
+  def show_all_cars(self):
+    sql = 'select * from car'
+    cars = cursor.execute(sql).fetchall()
+
+    self.show_cars(cars)
+
+  def show_cars(self, cars):
+      n = len(cars)
+      self.tb_car.setRowCount(n)
+      row = 0
+      for car in cars:  #car[0] => (1, 'Toyota', 'Yaris Cross', 2025, 867778)
+        self.tb_car.setItem(row, 0, QTableWidgetItem(str(car[0])))
+        self.tb_car.setItem(row, 1, QTableWidgetItem(car[1]))
+        self.tb_car.setItem(row, 2, QTableWidgetItem(car[2]))
+        self.tb_car.setItem(row, 3, QTableWidgetItem(str(car[3])))
+        self.tb_car.setItem(row, 4, QTableWidgetItem(str(car[4])))
+
+        row += 1
 
   def insert_car(self):
     brand = self.txt_brand.text()
@@ -35,6 +125,7 @@ class MainWindow(QMainWindow):
       QMessageBox.information(self, 
                               'Information', 
                               'Insert car successful!')
+      self.show_all_cars()
     else:
       QMessageBox.warning(self, 
                               'Warning', 
@@ -47,6 +138,20 @@ class MainWindow(QMainWindow):
     self.txt_model.setText('')
     self.txt_year.setText('')
     self.txt_price.setText('')
+
+    self.txt_brand.setEnabled(True)
+    self.txt_model.setEnabled(True)
+    self.txt_year.setEnabled(True)
+
+    self.tb_car.clearSelection()
+
+    self.btn_add.setEnabled(True)
+    self.btn_update.setEnabled(False)
+    self.btn_delete.setEnabled(False)
+
+    self.show_all_cars()
+
+
   
 if __name__ == '__main__':
   app = QApplication(sys.argv)
